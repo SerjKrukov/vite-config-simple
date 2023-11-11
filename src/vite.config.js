c6b34c4274b69327e3915a09ee6fbd3b38c9ab93
Serj Krukov <serj.krukov@gmail.com>
2023-11-11 14:13:54 +0200

    partials and vars

--- src/vite.config.js
+++ src/vite.config.js
@@ -1,8 +1,26 @@
 // vite.config.js
 import { resolve } from 'path'
 import { defineConfig } from 'vite'
+import handlebars from 'vite-plugin-handlebars';
+
+const pageData = {
+  '/index.html': {
+    title: 'Main Page',
+  },
+  '/det/index.html': {
+    title: 'Sub Page',
+  },
+};
 
 export default defineConfig({
+    plugins: [
+    handlebars({
+      context(pagePath) {
+        return pageData[pagePath];
+      },
+      partialDirectory: resolve(__dirname, 'partials'),
+    }),
+  ],
   build: {
     rollupOptions: {
       input: {
