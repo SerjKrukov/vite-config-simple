b3a12195d80ffec35b1ed465598832239dbed3d4
Serj Krukov <serj.krukov@gmail.com>
2023-12-09 13:09:19 +0200

    prod serve

--- src/vite.config.js
+++ src/vite.config.js
@@ -1,32 +1,32 @@
 // vite.config.js
-import { resolve } from 'path'
-import { defineConfig } from 'vite'
+import {resolve} from 'path'
+import {defineConfig} from 'vite'
 import handlebars from 'vite-plugin-handlebars';
 
 const pageData = {
-  '/index.html': {
-    title: 'Main Page',
-  },
-  '/det/index.html': {
-    title: 'Sub Page',
-  },
+    '/index.html': {
+        title: 'Main Page',
+    },
+    '/det/index.html': {
+        title: 'Sub Page',
+    },
 };
 
 export default defineConfig({
     plugins: [
-    handlebars({
-      context(pagePath) {
-        return pageData[pagePath];
-      },
-      partialDirectory: resolve(__dirname, 'partials'),
-    }),
-  ],
-  build: {
-    rollupOptions: {
-      input: {
-        main: resolve(__dirname, 'index.html'),
-        det: resolve(__dirname, 'det/index.html'),
-      },
+        handlebars({
+            context(pagePath) {
+                return pageData[pagePath];
+            },
+            partialDirectory: resolve(__dirname, 'partials'),
+        }),
+    ],
+    build: {
+        rollupOptions: {
+            input: {
+                main: resolve(__dirname, 'index.html'),
+                det: resolve(__dirname, 'det/index.html'),
+            },
+        },
     },
-  },
 })
